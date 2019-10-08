0659cd7348ad1429a96232e13c79ce02c0437902
qwe7002 <qwe7002@hotmail.com>
2019-10-09 01:33:59 +0800

    修改断网实现

--- app/src/main/java/com/qwe7002/telegram_rc/public_func.java
+++ app/src/main/java/com/qwe7002/telegram_rc/public_func.java
@@ -79,7 +79,6 @@ class public_func {
         return dual_sim;
     }
 
-    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     static boolean check_network_status(Context context) {
         ConnectivityManager manager = (ConnectivityManager) context
                 .getApplicationContext().getSystemService(
