3773f60a16d7db3a69fd90e2341488a1adb5fa97
qwe7002 <qwe7002@hotmail.com>
2019-10-23 00:34:28 +0800

    修改实现service

--- app/src/main/java/uk/reall/root_kit/activity_manage.java
+++ app/src/main/java/uk/reall/root_kit/activity_manage.java
@@ -1,8 +1,8 @@
 package uk.reall.root_kit;
 
 public class activity_manage {
+    @SuppressWarnings("SpellCheckingInspection")
     public static void start_service(String package_name, String service_name) {
         root_kit.run_shell_command("am startservice -n " + package_name + "/" + service_name);
     }
-
 }
