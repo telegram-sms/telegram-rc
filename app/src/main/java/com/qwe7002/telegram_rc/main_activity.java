32b09bf5bda82c9bbae505d3770d22e2f86f711a
qwe7002 <qwe7002@hotmail.com>
2019-11-20 04:52:44 +0800

    避免无卡时错误判断

--- app/src/main/java/com/qwe7002/telegram_rc/main_activity.java
+++ app/src/main/java/com/qwe7002/telegram_rc/main_activity.java
@@ -169,7 +169,7 @@ public class main_activity extends AppCompatActivity {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
                 assert tm != null;
-                if (tm.getPhoneCount() == 1) {
+                if (tm.getPhoneCount() <= 1) {
                     display_dual_sim_display_name.setVisibility(View.GONE);
                 }
             } else {
@@ -492,7 +492,7 @@ public class main_activity extends AppCompatActivity {
                     if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
                         TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
                         assert tm != null;
-                        if (tm.getPhoneCount() == 1) {
+                        if (tm.getPhoneCount() <= 1) {
                             display_dual_sim_display_name.setVisibility(View.GONE);
                         }
                         if (public_func.get_active_card(context) < 2) {
