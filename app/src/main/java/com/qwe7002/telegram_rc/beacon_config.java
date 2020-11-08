c96548c56b78761dd6f0ab0861aa936962fecb73
qwe7002 <qwe7002@hotmail.com>
2020-11-08 20:46:32 +0800

    修改默认值

--- app/src/main/java/com/qwe7002/telegram_rc/beacon_config.java
+++ app/src/main/java/com/qwe7002/telegram_rc/beacon_config.java
@@ -3,7 +3,7 @@ package com.qwe7002.telegram_rc;
 public class beacon_config {
     boolean enable = false;
     boolean use_vpn_hotspot = false;
-    Long delay = 1000L;
-    int disable_count = 10;
-    int enable_count = 20;
+    Long delay = 10000L;
+    int disable_count = 3;
+    int enable_count = 5;
 }
