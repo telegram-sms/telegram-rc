f161f7d1a6d3699d9ab19a77157aa484073bab39
qwe7002 <qwe7002@hotmail.com>
2020-12-09 20:03:21 +0800

    修改获取slot的方法

--- app/src/main/java/com/qwe7002/telegram_rc/chat_command_service.java
+++ app/src/main/java/com/qwe7002/telegram_rc/chat_command_service.java
@@ -543,8 +543,11 @@ public class chat_command_service extends Service {
                             public_func.send_sms(context, msg_send_to, msg_send_content.toString(), -1, -1);
                             return;
                         }
-                        int slot = -1;
-                        switch (command) {
+                        int slot = 0;
+                        if (command.equals("/sendsms2")) {
+                            slot = 1;
+                        }
+                        /*switch (command) {
                             case "/sendsms":
                             case "/sendsms1":
                                 slot = 0;
@@ -552,7 +555,7 @@ public class chat_command_service extends Service {
                             case "/sendsms2":
                                 slot = 1;
                                 break;
-                        }
+                        }*/
                         int sub_id = public_func.get_sub_id(context, slot);
                         if (sub_id != -1) {
                             public_func.send_sms(context, msg_send_to, msg_send_content.toString(), slot, sub_id);
@@ -562,16 +565,10 @@ public class chat_command_service extends Service {
                 } else {
                     has_command = false;
                     send_sms_next_status = SEND_SMS_STATUS.PHONE_INPUT_STATUS;
-                    int send_slot = -1;
+                    int send_slot = 0;
                     if (public_func.get_active_card(context) > 1) {
-                        switch (command) {
-                            case "/sendsms":
-                            case "/sendsms1":
-                                send_slot = 0;
-                                break;
-                            case "/sendsms2":
-                                send_slot = 1;
-                                break;
+                        if (command.equals("/sendsms2")) {
+                            send_slot = 1;
                         }
                     }
                     Paper.book("send_temp").write("slot", send_slot);
