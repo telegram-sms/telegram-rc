ef12ccf8d26e14da0eaca7ddd26e0e4b8d8c40b5
qwe7002 <qwe7002@hotmail.com>
2020-12-19 01:13:36 +0800

    只在没有消息的时候休眠

--- app/src/main/java/com/qwe7002/telegram_rc/battery_service.java
+++ app/src/main/java/com/qwe7002/telegram_rc/battery_service.java
@@ -81,10 +81,13 @@ public class battery_service extends Service {
                 }
                 send_loop_list.removeAll(need_remove);
                 need_remove.clear();
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                if (send_loop_list.size() == 0) {
+                    //Only enter sleep mode when there are no messages
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }).start();
