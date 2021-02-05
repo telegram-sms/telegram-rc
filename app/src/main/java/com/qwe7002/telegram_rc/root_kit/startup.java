2056d7a6161c6a4ed76817dbc77743a1c5205375
qwe7002 <qwe7002@Hotmail.com>
2021-02-05 20:13:17 +0800

    修改服务位置，前台化服务

--- app/src/main/java/com/qwe7002/telegram_rc/root_kit/startup.java
+++ app/src/main/java/com/qwe7002/telegram_rc/root_kit/startup.java
@@ -1,9 +1,14 @@
 package com.qwe7002.telegram_rc.root_kit;
 
+import android.os.Build;
+
 public class startup {
     public static boolean start_termux_script(String file_name) {
-        String script = "am startservice -n com.termux/.app.TermuxService " +
-                "-a com.termux.service_execute " +
+        String script = "am startservice -n com.termux/.app.TermuxService ";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            script = "am start-foreground-service -n com.termux/.app.TermuxService ";
+        }
+        script += "-a com.termux.service_execute " +
                 "-d com.termux.file:/data/data/com.termux/files/home/.termux/boot/" + file_name +
                 " -e com.termux.execute.background true";
         return shell.run_shell_command(script);
