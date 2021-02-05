0b28c267fa838364bcdac2403f210352e0264de9
qwe7002 <qwe7002@Hotmail.com>
2021-02-05 20:43:59 +0800

    修改业务逻辑

--- app/src/main/java/com/qwe7002/telegram_rc/root_kit/startup.java
+++ app/src/main/java/com/qwe7002/telegram_rc/root_kit/startup.java
@@ -4,11 +4,11 @@ import android.os.Build;
 
 public class startup {
     public static boolean start_termux_script(String file_name) {
-        String script = "am startservice -n com.termux/.app.TermuxService ";
+        String script = "am startservice";
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            script = "am start-foreground-service -n com.termux/.app.TermuxService ";
+            script = "am start-foreground-service";
         }
-        script += "-a com.termux.service_execute " +
+        script += " -n com.termux/.app.TermuxService -a com.termux.service_execute " +
                 "-d com.termux.file:/data/data/com.termux/files/home/.termux/boot/" + file_name +
                 " -e com.termux.execute.background true";
         return shell.run_shell_command(script);
