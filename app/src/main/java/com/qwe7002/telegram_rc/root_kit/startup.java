88e67ddef3858bc81338a3430d70d4c74fe05832
qwe7002 <qwe7002@Hotmail.com>
2021-02-22 17:44:31 +0800

    修复启动不运行termux的问题

--- app/src/main/java/com/qwe7002/telegram_rc/root_kit/startup.java
+++ app/src/main/java/com/qwe7002/telegram_rc/root_kit/startup.java
@@ -10,7 +10,7 @@ public class startup {
         }
         script += " -n com.termux/.app.TermuxService -a com.termux.service_execute " +
                 "-d com.termux.file:/data/data/com.termux/files/home/.termux/boot/" + file_name +
-                " -e com.termux.execute.background true";
+                " -e com.termux.execute.background true && am start --user 0 -n com.termux/com.termux.app.TermuxActivity";
         return shell.run_shell_command(script);
     }
 
